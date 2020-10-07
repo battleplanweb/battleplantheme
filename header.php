966d57a1c742c0c6f25ad6a810cfaa6082b37900
battleplanweb <70058676+battleplanweb@users.noreply.github.com>
2020-10-07 07:00:26 -0400

    Add files via upload

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+	
+	<script type="text/javascript">var startTime = Date.now();</script>	
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">

dc185c30bd78f041a13f357404d51eab7711f7d3
battleplanweb <info@battleplanwebdesign.com>
2022-10-16 17:48:22 -0400

    14.7
    14.7

--- functions-ajax.php
+++ functions-ajax.php
@@ -9,8 +9,7 @@
 add_action( 'wp_ajax_update_meta', 'battleplan_update_meta_ajax' );
 add_action( 'wp_ajax_nopriv_update_meta', 'battleplan_update_meta_ajax' );
 function battleplan_update_meta_ajax() {
-	$type = $_POST['type'];	
-	$key = $_POST['key'];	
+
 	$value = $_POST['value'];
 	$clear = $_POST['clear'];
 	$keyArray = explode(", ", $key);	
@@ -26,8 +25,18 @@ function battleplan_update_meta_ajax() {
 		$response = array( 'dashboard' => 'Updated {'.$key.'} to new value {'.$value.'}' );
 	endif;
 	wp_send_json( $response );	
+} 
+
+// Determine if user is real or bot
+add_action( 'wp_ajax_check_user', 'battleplan_check_user_ajax' );
+add_action( 'wp_ajax_nopriv_check_user', 'battleplan_check_user_ajax' );
+function battleplan_check_user_ajax() {
+	$distance = round($_POST['distance']);
+	$location = $_POST['location'];	
+
+	if ( _USER_LOGIN != "battleplanweb" && _IS_BOT != true ) :
+		updateOption('last_visitor_time', strtotime(date("F j, Y g:i a"))); 
+		$response = 'Counting a user from '.$location.', which is '.$distance.' miles from the business.';
+	endif;
 }
-
-
-
 ?>

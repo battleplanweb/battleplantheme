5fad6ae647411f55f77fa961157163f2c61b1752
battleplanweb <info@battleplanwebdesign.com>
2022-11-30 14:18:58 -0500

    16.0.2
    16.0.2

--- js/script-desktop-raw.js
+++ js/script-desktop-raw.js
@@ -176,8 +176,8 @@ document.addEventListener("DOMContentLoaded", function () {	"use strict"; (funct
 	
 // Reveal "Are We Open" banner
 setTimeout(function() {
-	var posY = getPosition ($('#masthead .phone-link'), 'center-y', 'window'), posX = getPosition ($('#masthead .phone-link'), 'right', 'window'); 
-	if ( posX > 1200 ) { $('.currently-open-banner').addClass('horz'); }
+	var posY = getPosition ($('#masthead .phone-link'), 'center-y', 'window'), posX = getPosition ($('#masthead .phone-link'), 'right', 'window'), breakpoint = getDeviceW() - posX;
+	if ( breakpoint < 133 ) { $('.currently-open-banner').addClass('horz'); }
 	$('.currently-open-banner').css({"top":posY+"px", "left":posX+"px"});
 	$('.currently-open-banner').addClass('reveal-open');
 }, 2000);

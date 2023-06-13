e542e1ad637f61514139bf278203223637a633d5
battleplanweb <info@battleplanwebdesign.com>
2023-06-13 14:48:54 -0400

    19.5
    19.5

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@
 /*--------------------------------------------------------------
 # Set Constants
 --------------------------------------------------------------*/
-if ( !defined('_BP_VERSION') ) define( '_BP_VERSION', '19.4' );
+if ( !defined('_BP_VERSION') ) define( '_BP_VERSION', '19.5' );
 update_option( 'battleplan_framework', _BP_VERSION, false );
 
 if ( !defined('_BP_NONCE') ) define( '_BP_NONCE', base64_encode(random_bytes(20)) );

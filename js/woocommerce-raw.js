12314a7d8e54862b6600879968a797e3a8ad9248
battleplanweb <info@battleplanwebdesign.com>
2023-08-15 10:15:36 -0400

    20.9
    20.9

--- js/woocommerce-raw.js
+++ js/woocommerce-raw.js
@@ -3,27 +3,27 @@ document.addEventListener("DOMContentLoaded", function () {	"use strict"; (funct
 	$(window).on("load", function() {
 		
 	// Set up woocommerce pages to match rest of site
-		$('.woocommerce-page main#main').unwrap();
-		$('.woocommerce-page main#main').attr('id', 'primary');		
+		$('.woocommerce-js main#main').unwrap();
+		$('.woocommerce-js main#main').attr('id', 'primary');		
 		
 	// Set up products in CSS grid		
-		$('.theme-battleplantheme.woocommerce-page ul.products.columns-2').addClass('grid-2e');
-		$('.theme-battleplantheme.woocommerce-page ul.products.columns-3').addClass('grid-3e');
-		$('.theme-battleplantheme.woocommerce-page ul.products.columns-4').addClass('grid-4e');
-		$('.theme-battleplantheme.woocommerce-page ul.products.columns-5').addClass('grid-5e');
-		$('.theme-battleplantheme.woocommerce-page ul.products.columns-6').addClass('grid-6e');
-		$('.theme-battleplantheme.woocommerce-page ul.products.columns-7').addClass('grid-7e');
-		$('.theme-battleplantheme.woocommerce-page ul.products.columns-8').addClass('grid-8e');
+		$('.theme-battleplantheme.woocommerce-js ul.products.columns-2').addClass('grid-2e');
+		$('.theme-battleplantheme.woocommerce-js ul.products.columns-3').addClass('grid-3e');
+		$('.theme-battleplantheme.woocommerce-js ul.products.columns-4').addClass('grid-4e');
+		$('.theme-battleplantheme.woocommerce-js ul.products.columns-5').addClass('grid-5e');
+		$('.theme-battleplantheme.woocommerce-js ul.products.columns-6').addClass('grid-6e');
+		$('.theme-battleplantheme.woocommerce-js ul.products.columns-7').addClass('grid-7e');
+		$('.theme-battleplantheme.woocommerce-js ul.products.columns-8').addClass('grid-8e');
 		
 	// Reset breadcrumbs to common look
 		$('nav.woocommerce-breadcrumb').addClass('breadcrumbs').removeClass('woocommerce-breadcrumb');
 				
 	// Reset pagination buttons to common look	
-		replaceText('.theme-battleplantheme.woocommerce-page nav.woocommerce-pagination ul li a.prev', '', '<i class="fa fa-chevron-left"></i>', 'html');
-		replaceText('.theme-battleplantheme.woocommerce-page nav.woocommerce-pagination ul li a.next', '', '<i class="fa fa-chevron-right"></i>', 'html');
+		replaceText('.theme-battleplantheme.woocommerce-js nav.woocommerce-pagination ul li a.prev', '', '<i class="fa fa-chevron-left"></i>', 'html');
+		replaceText('.theme-battleplantheme.woocommerce-js nav.woocommerce-pagination ul li a.next', '', '<i class="fa fa-chevron-right"></i>', 'html');
 		 
 	// Change product page to CSS grid
-		/*$('.theme-battleplantheme.woocommerce-page div.product').addClass('flex').addClass('grid-1-1');*/
+		/*$('.theme-battleplantheme.woocommerce-js div.product').addClass('flex').addClass('grid-1-1');*/
 		
 	// On non-variation products, wrap Quantity input and Add To Cart button 
 		$('.woocommerce-variation-add-to-cart').addClass("bp-add-to-cart");

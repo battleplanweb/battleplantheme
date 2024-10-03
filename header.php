79c32916ca8a829d15c3fb147afbcdea2aa39690
battleplanweb <info@battleplanwebdesign.com>
2024-10-03 13:13:17 -0400

    2024.25.3
    2024.25.3

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 		header( "X-Frame-Options: SAMEORIGIN" );
 		header( "X-Content-Type-Options: nosniff" );
 		header( "Referrer-Policy: strict-origin-when-cross-origin" );
-	endif; 
+	endif;  
 ?> 
  
 <html lang="en">

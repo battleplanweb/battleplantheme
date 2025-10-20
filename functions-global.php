922f584898b4cfae571f589e05eaba4b5e8497fc
battleplanweb <info@battleplanwebdesign.com>
2025-10-20 16:10:02 -0400

    2025.32.12
    2025.32.12

--- functions-global.php
+++ functions-global.php
@@ -14,7 +14,7 @@
 --------------------------------------------------------------*/
 
 
-if ( !defined('_BP_VERSION') ) define( '_BP_VERSION', '2025.32.11' );
+if ( !defined('_BP_VERSION') ) define( '_BP_VERSION', '2025.32.12' );
 update_option( 'battleplan_framework', _BP_VERSION, false );
 
 if ( !defined('_BP_NONCE') ) define( '_BP_NONCE', base64_encode(random_bytes(20)) );
